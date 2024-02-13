9cd9282690892a5c32e6fb0c0739940a187ae865
Caleb Neujahr <caleb.neujahr@mymail.champlain.edu>
2024-02-13 14:25:50 -0500

    Finished YouTube Tutorials!

---
+++ CNeujahr_Platformer/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private bool _facingRight = true;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_facingRight && rb.velocity.x < -0.1)
+        {
+            Flip();
+        }
+        else if(!_facingRight && rb.velocity.x > 0.1)
+        {
+            Flip();
+        }
+
+        animator.SetFloat("MoveSpeedX", Mathf.Abs(rb.velocity.x) / playerMovement.XSpeed);
+        animator.SetBool("Grounded", playerMovement.IsGrounded);
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
---
+++ CNeujahr_Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float xSpeed = 10f;
+    public float XSpeed => xSpeed;
+    [SerializeField] private float jumpForce = 880f;
+    [SerializeField] private float groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D _rb;
+
+    private float _xMoveInput;
+
+    private bool _shouldJump;
+    private bool _isGrounded;
+    public bool IsGrounded => _isGrounded;
+
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _xMoveInput = Input.GetAxis("Horizontal") * xSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _shouldJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Collider2D col = Physics2D.OverlapCircle(transform.position, groundCheckRadius, groundLayer);
+        _isGrounded = col != null;
+        _rb.velocity = new Vector2(_xMoveInput, _rb.velocity.y);
+        if(_shouldJump)
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector2.up * jumpForce);
+            }
+            _shouldJump = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _isGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.SetParent(other.transform, true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.SetParent(null, true);
+        }
+    }
+}
