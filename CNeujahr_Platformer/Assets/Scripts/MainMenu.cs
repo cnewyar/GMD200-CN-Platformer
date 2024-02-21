b4ae1505cd109923075607496756ac41e8f0ba91
Caleb Neujahr <caleb.neujahr@mymail.champlain.edu>
2024-02-20 23:19:20 -0500

    Game Complete!

---
+++ CNeujahr_Platformer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Level 1");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadSceneAsync("Level 1");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
